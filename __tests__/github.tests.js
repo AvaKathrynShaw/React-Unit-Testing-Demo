f457f33ee5e1f91065af8136d0c045d0ee502037
Ava Shaw <avashaw@Macintosh.local>
2017-07-25 10:59:49 -0400

    Adding Test Folders

---
+++ __tests__/github.tests.js
@@ -0,0 +1,15 @@
+/* eslint-env jest */
+
+const github = require('../github')
+jest.mock(‘../request’)
+
+// A simple example test
+describe('#getUser() using Promises', () => {
+  it('should load user data', () => {
+    return github.getUser('vnglst')
+    .then(data => {
+      expect(data).toBeDefined()
+      expect(data.entity.name).toEqual('Koen van Gilst')
+    })
+  })
+})
